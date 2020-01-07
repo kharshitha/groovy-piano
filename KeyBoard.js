3c231048f4cf29a2fb9f998b15389c687cde6419
kharshitha <59593522+kharshitha@users.noreply.github.com>
2020-01-07 12:03:52 +0530

    Update KeyBoard.js

--- KeyBoard.js
+++ KeyBoard.js
@@ -1,23 +1,39 @@
 var listOfKeys = [];
-var element;
-document.onkeydown = function (e) {
-    element = e.key.toLowerCase();
+var i = 0;
+document.onkeydown = function Sound(e) {
+    var element = e.key.toLowerCase();
     document.getElementById(element).click();
-    listOfKeys.push(element.toUpperCase());
     document.getElementById(element).classList.add('clicked');
     setTimeout(function () { document.getElementById(element).classList.remove('clicked'); }, 100);
+    if (document.getElementById('recordBtn').innerHTML == "Recording.....") {
+        listOfKeys.push(element.toUpperCase());
+    }
 }
-function store() {
-    var element=document.getElementById('recordbtn');
+function storing() {
     localStorage.setItem("Keys", JSON.stringify(listOfKeys));
-    document.getElementById('recordbtn').innerHTML="Recorded";
-    textarea.innerHTML=localStorage.getItem("Keys");
+    document.getElementById('recordBtn').innerHTML = "Record";
+    alert("keys are recorded");
 }
+function recording() {
+    listOfKeys.clear;
+    document.getElementById('recordBtn').innerHTML = "Recording.....";
+    alert("keys are being recorded");
+}
+
 function playNow(ctrl) {
     new Audio(`${ctrl.getAttribute("data-audio")}.wav`).play();
-    storing(this);
 }
-function storageElements()
-{
-    //textarea.innerHTML=localStorage.getItem("Keys");
+function play() {
+    var len = listOfKeys.length;
+    if (i < len) {
+        var element = listOfKeys[i].toLowerCase();
+        //debugger;
+        document.getElementById(element).click();
+        document.getElementById(element).classList.add('clicked');
+        setTimeout(function () { document.getElementById(element).classList.remove('clicked'); }, 1000);
+        i = i + 1;
+        setTimeout(function () { play(); }, 1000);
+    }
+
 }
+
